d02fe15b3cac47121eb1ed7843ac1b849927e8e2
William Roger <williamroger.frontend@gmail.com>
2021-09-20 23:08:55 -0300

    Estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 function App() {
   return (
     <div>
-      <h1>Dogs started...</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
