464391aee8e3f674e49114e397c77d5ff27ae6f7
William Roger <williamroger.frontend@gmail.com>
2021-09-24 02:08:49 -0300

    Adicionado Context API

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import { UserContext } from '../UserContext'; 
 
 const Header = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login" aria-label="login">Login / Criar</Link>
+        {data ? (
+          <Link className={styles.login} to="/conta" aria-label="login">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login" aria-label="login">
+            Login / Criar
+          </Link>
+        )}
+        
       </nav>
     </header>
   );
