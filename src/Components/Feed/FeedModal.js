4c24af67a9a9793cb9c7d10f17ce852bc544a069
William Roger <williamroger.frontend@gmail.com>
2021-10-10 03:08:12 -0300

    Adicionado componente ModalPhoto

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useFetch from '../../Hooks/useFetch';
+import styles from './FeedModal.module.css';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
+import { PHOTO_GET } from '../../api';
 
-const FeedModal = () => {
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const {data, error, loading, request} = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    // console.log('target ', event.target);
+    // console.log('current ', event.currentTarget);
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+  
   return (
-    <div></div>
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
   );
 }
 
--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Error from '../../Components/Helper/Error';
 import Loading from '../../Components/Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
@@ -24,7 +24,7 @@ const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map(photo => (
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />
         ))}
       </ul>
     );
